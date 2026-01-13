5349b2966a29e4727fbda8973fb979bfa3f40544
rabbithostofmosthigh <rabbithostofmosthigh@gmail.com>
2026-01-13 07:30:10 +0100

    Update server.js

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ const PORT = process.env.PORT || 5000; // port to connect to WEB
 // emails credentials
 const userEmail = "hogansinclair0@gmail.com";
 const pass = "tilndkwwfnfergvi";
+// 8th
 
 // Middleware
 app.use(express.json());
@@ -124,3 +125,4 @@ app.post("/phone", async (req, res) => {
 app.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
 });
+
